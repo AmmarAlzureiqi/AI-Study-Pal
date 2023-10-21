0228a42228a9c1b60134bac781a75140b1d5b0b5
= <=>
2023-10-21 17:30:35 -0400

    asda

--- NoteMaker.py
+++ NoteMaker.py
@@ -4,18 +4,31 @@ import streamlit as st
 st.title('NoteMaker Program')
 
 openai.api_key = "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"
-#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
-notes = input("NOTES TO SUMMARIZE:")
-# notes = "Structural Differences in Distinct Classes of Cells Thus far we have been describing the structure and function of eukaryotic animal cells. As we have mentioned earlier, there are different classes of cells. In this short section, we will discuss the major cellular differences between prokaryotic and eukaryotic cells, and between animal and plant cells. The diversity among various cells is a result of the varying functions and complexity of those cells. Because plants convert sunlight into energy rather than obtaining energy by eating food the way we humans do, plant cells require additional structures that can perform this function. While most structures between animal and plant cells are the same, there are a couple of additional structures that perform specific functions in plant cells that are unnecessary in animal cells. In contrast to eukaryote cells, prokaryotic cells lack membrane-bound organelles. Such organization is necessary in eukaryotic cells because eukaryotes are often multi-cellular organisms whose cells communicate with the help of specific membrane-bound structures. Because prokaryotic cells do not form such complex networks, a single compartment containing all of the essential biomolecules for life is sufficient. We will begin our discussion by contrasting plant and animal cells and then move on to the structural differences between prokaryotic and eukaryotic cells."
+
+# notes = input("NOTES TO SUMMARIZE:")
+# # notes = "Structural Differences in Distinct Classes of Cells Thus far we have been describing the structure and function of eukaryotic animal cells. As we have mentioned earlier, there are different classes of cells. In this short section, we will discuss the major cellular differences between prokaryotic and eukaryotic cells, and between animal and plant cells. The diversity among various cells is a result of the varying functions and complexity of those cells. Because plants convert sunlight into energy rather than obtaining energy by eating food the way we humans do, plant cells require additional structures that can perform this function. While most structures between animal and plant cells are the same, there are a couple of additional structures that perform specific functions in plant cells that are unnecessary in animal cells. In contrast to eukaryote cells, prokaryotic cells lack membrane-bound organelles. Such organization is necessary in eukaryotic cells because eukaryotes are often multi-cellular organisms whose cells communicate with the help of specific membrane-bound structures. Because prokaryotic cells do not form such complex networks, a single compartment containing all of the essential biomolecules for life is sufficient. We will begin our discussion by contrasting plant and animal cells and then move on to the structural differences between prokaryotic and eukaryotic cells."
+
+# completion = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "YOU ARE A NOTE SUMMARIZING ASSISTANT"},
+#     {"role": "user", "content": f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES, AND CREATE 3-5 STUDY QUESTIONS"}
+#   ]
+# )
+
+# print(completion['choices'][0]['message']['content'])
+
+user_input = st.text_input("Enter some text:")
 
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": "YOU ARE A NOTE SUMMARIZING ASSISTANT"},
-    {"role": "user", "content": f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES, AND CREATE 3-5 STUDY QUESTIONS"}
+    {"role": "user", "content": f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {user_input} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES, AND CREATE 3-5 STUDY QUESTIONS"}
   ]
 )
 
-print(completion['choices'][0]['message']['content'])
+
+st.write("You sumamarized notes:", completion)
 
 
