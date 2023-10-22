b9b08f116c516efb2e67d08847bf6ef1095600da
= <=>
2023-10-22 09:40:36 -0400

    update NoteMaker.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import streamlit as st
+import os
 import openai
-import NoteMaker
+from NoteMaker import generate_summary_and_questions
 
 
 openai.api_key = st.secrets["API_KEY"]
-st.title("Educational Notes Summarizer and Study Question Generator1")
+st.title("Educational Notes Summarizer and Study Question Generator")
 user_notes = st.text_area("Enter your educational notes:")
 generate_summary = st.checkbox(f"Generate Summary (check to generate 5 study questions)")
 
 # Generate summary or study questions when the user submits the notes
 if st.button("Generate"):
     if user_notes:
-        generated_text = NoteMaker.generate_summary_and_questions(user_notes, generate_summary)
+        generated_text = generate_summary_and_questions(user_notes, generate_summary)
         st.subheader("Output:")
         st.write(generated_text)
     else:
