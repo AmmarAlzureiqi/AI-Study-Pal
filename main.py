01e22cd6b3ee39738083c67c9b9c1748c5f77ac2
= <=>
2023-10-22 10:02:29 -0400

    update NoteMaker.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ generate_summary = st.checkbox(f"Generate 5 Study Questions")
 
 with st.spinner("Loading..."):
 # Generate summary or study questions when the user submits the notes
-if st.button("Generate"):
-    if user_notes:
-        generated_text = generate_summary_and_questions(user_notes, generate_summary)
-        st.subheader("Output:")
-        st.write(generated_text)
-    else:
-        st.warning("Please enter your notes before generating the output.")
+    if st.button("Generate"):
+        if user_notes:
+            generated_text = generate_summary_and_questions(user_notes, generate_summary)
+            st.subheader("Output:")
+            st.write(generated_text)
+        else:
+            st.warning("Please enter your notes before generating the output.")
 st.success("Done!")
 
 with st.sidebar:
