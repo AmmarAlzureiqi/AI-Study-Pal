605af274bbf384590fe1d1488793b17e6bb67117
= <=>
2023-10-22 10:45:29 -0400

    update NoteMaker.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import os
 import openai
 from NoteMaker import generate_summary_and_questions
-from StudyPlan import generate_studyplan
 
 openai.api_key = st.secrets["API_KEY"]
 st.title("Notes Summary and Study Question Generator")
@@ -21,13 +20,6 @@ with st.spinner("Loading..."):
         else:
             st.warning("Please enter your notes before generating the output.")
 
-if st.button("Generate"):
-    if user_notes2:
-        generated_text2 = generate_studyplan(user_notes2)
-        st.subheader("Output:")
-        st.write(generated_text2)
-        st.success("Done!")
-    else:
-        st.warning("Please enter your notes before generating the output.")
+
 
 
