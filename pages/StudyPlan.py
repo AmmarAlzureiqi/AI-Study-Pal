3a903691a17e1d43a37498a189feaae4845a4944
= <=>
2023-10-22 12:08:06 -0400

    updaet NoteMaker.py

--- pages/StudyPlan.py
+++ pages/StudyPlan.py
@@ -18,7 +18,7 @@ def generate_studyplan(notes, days):
 
 openai.api_key = st.secrets["API_KEY"]
 st.title("Study Plan Generator")
-days_left = st.text_area("How many days for the study plan?:", value="Ex: 10 Days", height=20)
+days_left = st.text_area("How many days for the study plan?:", value="Ex: 10 Days", height=50)
 user_notes = st.text_area("Enter your notes:")
 
 with st.spinner("Loading..."):
