e5c781d3531feddbf86dad0be4af026f036fcc61
= <=>
2023-10-22 11:25:57 -0400

    update NoteMaker.py

--- pages/NoteMaker.py
+++ pages/NoteMaker.py
@@ -1,30 +1,9 @@
 import streamlit as st
-import os
 import openai
 
+def generate_studyplan(notes):
+    prompt = f"Can you make me a study plan for these I have 2 weeks: {notes}"
 
-#openai.api_key = st.text_area("Enter API Key")
-openai.api_key = st.secrets["API_KEY"]
-
-def generate_summary_and_questions(notes, generate_questions=True, generate_links=False):
-    prompt = f"Summarize the following notes: {notes}"
-    if generate_questions and generate_links:
-        prompt = f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES, AND CREATE 3-5 STUDY QUESTIONS and include helpful 2 helpful websites"
-    elif generate_questions:
-        prompt = f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES, AND CREATE 3-5 STUDY QUESTIONS"
-    elif generate_links:
-        prompt = f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES and include helpful 2 helpful websites"
-    else:
-        prompt = f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES"
-
-        
-    
-    #response = openai.Completion.create(
-        #engine="text-davinci-003",
-        #prompt=prompt,
-        #max_tokens=250,  # Adjust the max_tokens based on your requirements
-        #n=1,
-    #)
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
@@ -36,23 +15,23 @@ def generate_summary_and_questions(notes, generate_questions=True, generate_link
     generated_text = response['choices'][0]['message']['content'] #response.choices[0].text.strip()
     return generated_text
 
-# # Streamlit app
-# st.title("Educational Notes Summarizer and Study Question Generator")
 
-# # Text input for user's notes
-# user_notes = st.text_area("Enter your educational notes:")
-
-# # Checkbox to choose between summary and study questions
-# generate_summary = st.checkbox(f"Generate Summary (check to generate 5 study questions)")
+openai.api_key = st.secrets["API_KEY"]
+st.title("Notes Summary and Study Question Generator")
+user_notes = st.text_area("Enter your educational notes:")
+generate_summary = st.checkbox(f"Generate 5 Study Questions")
+generate_links = st.checkbox(f"Suggest Helpful Resources")
 
 
-# # Generate summary or study questions when the user submits the notes
-# if st.button("Generate"):
-#     if user_notes:
-#         generated_text = generate_summary_and_questions(user_notes, generate_summary)
-#         st.subheader("Output:")
-#         st.write(generated_text)
-#     else:
-#         st.warning("Please enter your notes before generating the output.")
+with st.spinner("Loading..."):
+    if st.button("Generate"):
+        if user_notes:
+            generated_text = generate_studyplan(user_notes)
+            st.subheader("Output:")
+            st.write(generated_text)
+            st.success("Done!")
+        else:
+            st.warning("Please enter your notes before generating the output.")
+
 
 
