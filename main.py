ab791c7ef4441650bfc3c0426887136b9343b164
= <=>
2023-10-22 10:21:31 -0400

    update NoteMaker.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from st_pages import Page, show_pages, add_page_title
 
 add_page_title() # By default this also adds indentation
 
-Specify what pages should be shown in the sidebar, and what their titles and icons
-should be
+
 show_pages(
     [
         Page("streamlit_app.py", "Home", "🏠"),
