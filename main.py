2f4a51a7ec80de086ea433e52499b489c5f8adcc
= <=>
2023-10-22 11:32:52 -0400

    update NoteMaker.py

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 import streamlit as st
 import os
 import openai
-from AI_Summarizer import generate_summary_and_questions
-# from StudyPlan import generate_studyplan
 
 openai.api_key = st.secrets["API_KEY"]
-st.title("Notes Summary and Study Question Generator")
-user_notes = st.text_area("Enter your educational notes:")
-generate_summary = st.checkbox(f"Generate 5 Study Questions")
-generate_links = st.checkbox(f"Suggest Helpful Resources")
 
-with st.spinner("Loading..."):
-    if st.button("Generate"):
-        if user_notes:
-            generated_text = generate_summary_and_questions(user_notes, generate_summary, generate_links)
-            st.subheader("Output:")
-            st.write(generated_text)
-            st.success("Done!")
-        else:
-            st.warning("Please enter your notes before generating the output.")
+def generate_summary_and_questions(notes, generate_questions=True, generate_links=False):
+    prompt = f"Summarize the following notes: {notes}"
+    if generate_questions and generate_links:
+        prompt = f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES, AND CREATE 3-5 STUDY QUESTIONS and include helpful 2 helpful websites"
+    elif generate_questions:
+        prompt = f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES, AND CREATE 3-5 STUDY QUESTIONS"
+    elif generate_links:
+        prompt = f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES and include helpful 2 helpful websites"
+    else:
+        prompt = f"CAN YOU GIVE ME A SUMMARY OF THESE NOTES {notes} THAT IS 33% THE LENGTH OF THE ORIGINAL NOTES"
 
-# with st.spinner("Loading..."):
-#     if st.button("Generate"):
-#         if user_notes:
-#             generated_text = generate_studyplan(user_notes)
-#             st.subheader("Output:")
-#             st.write(generated_text)
-#             st.success("Done!")
-#         else:
-#             st.warning("Please enter your notes before generating the output.")
+        
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "YOU ARE A NOTE SUMMARIZING ASSISTANT"},
+        {"role": "user", "content": prompt}
+    ]
+    )
 
-
+    generated_text = response['choices'][0]['message']['content'] #response.choices[0].text.strip()
+    return generated_text
 
 
 
