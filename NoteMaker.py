ed8357c78452ce8e3529a9dd4b4891f9b5b90a0e
= <=>
2023-11-07 18:18:33 -0500

    update

--- NoteMaker.py
+++ NoteMaker.py
@@ -5,7 +5,7 @@ from st_pages import Page, show_pages, add_page_title
 
 st.set_page_config(page_title = "This is a Multipage WebApp", page_icon="📓") 
 st.title("This is the Home Page Geeks.") 
-st.sidebar.success("Select Any Page from here") 
+st.sidebar.success("Select Any Page from here1") 
 
 show_pages(
     [
