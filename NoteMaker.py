4af7f369fd5394d986939159ef13e7d46c41bcf1
= <=>
2023-11-07 18:21:18 -0500

    update

--- NoteMaker.py
+++ NoteMaker.py
@@ -3,9 +3,9 @@ import openai
 from AI_Summarizer import generate_summary_and_questions
 from st_pages import Page, show_pages, add_page_title
 
-st.set_page_config(page_title = "This is a Multipage WebApp", page_icon="📓") 
+st.set_page_config(page_title = "All in one AI Study Assistant", page_icon="📓") 
 st.title("This is the Home Page Geeks.") 
-st.sidebar.success("Select Any Page from here1") 
+st.sidebar.success("Select Any Page from here") 
 
 show_pages(
     [
