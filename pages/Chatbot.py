f6e4f3032038a90b721d9bfc7c1562859f118189
= <=>
2023-11-07 18:39:01 -0500

    update

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
 
 st.title("Your Personal Study Expert")
-topic = st.text_area("Enter study subject:")
+topic = st.text_area("Enter Study Subject: Whatever subject you enter here, your study bot will become an expert in!")
 
 
 
