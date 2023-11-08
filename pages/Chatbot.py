35947689e5c28eecf330ff8b36cfb54987acb20d
= <=>
2023-11-07 23:43:08 -0500

    update

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -1,28 +1,55 @@
 import openai
 import streamlit as st
 
+st.title("Your Personal Study Expert")
+
+st.subheader("Whatever subject you enter here, your study bot will become an expert in!")
+topic = st.text_area("Enter Study Subject: (Feel free to add what grade or year of study you are in as well)")
+
+
 openai.api_key = st.secrets["API_KEY"]
 
-def createChat(subject = 'biology'):
-    tempstring = f"You are a {subject} expert"
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": tempstring},
-    ]
-    )
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    generated_text = response['choices'][0]['message']['content'] #response.choices[0].text.strip()
-    return generated_text
+    
+
+if topic:
+    initialprompt = f"Hello, ChatGPT. I'd like to focus on {topic} and have you assist me in studying this field comprehensively. Please provide detailed explanations, summaries, and answer questions on various {topic} topics. Additionally,if I ask for clarification or further examples, kindly provide those as well. Let's delve deep into the world of {topic} to help me improve my understanding and knowledge in this subject."
+    st.session_state.messages.append({"role": "assistant", "content": initialprompt})
+
+for message in st.session_state.messages:
+    if message["role"] == "assistant" and message["content"] == initialprompt:
+        continue
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "assistant", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 
-st.title("Your Personal Study Expert")
-topic = st.text_area("Enter Study Subject: Whatever subject you enter here, your study bot will become an expert in!", 
-                     value= 'ie. Biology', height=50)
-
-tempoutput = createChat(subject = topic)
-st.write(tempoutput)
 
 
 
@@ -31,21 +58,5 @@ st.write(tempoutput)
 
 
 
-# openai.api_key = 
-
-# messages = []
-# system_msg = input("What type of chatbot would you like to create?\n")
-# messages.append({"role": "system", "content": system_msg})
-
-# print("Your new assistant is ready!")
-# while input != "quit()":
-#     message = input()
-#     messages.append({"role": "user", "content": message})
-#     response = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",
-#         messages=messages)
-#     reply = response["choices"][0]["message"]["content"]
-#     messages.append({"role": "assistant", "content": reply})
-#     print("\n" + reply + "\n")
 
 
